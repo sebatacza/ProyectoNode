9285f488797e3386c3f3a8d5f0cbd5c44b95d161
Sebastian <juansebastian.tacza@gmail.com>
2024-05-24 01:41:33 -0300

    mejora validacion correo, uso de objeto socio

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -53,6 +53,30 @@ class OtroProducto extends Productos{
     }
 }
 
+class Personas{
+    #id
+    #nombre
+    #apellido
+    #correo
+    #documento
+    constructor(id,nombre,apellido,correo,documento){
+        this.#id = id,
+        this.#nombre = nombre,
+        this.#apellido = apellido,
+        this.#correo = correo,
+        this.#documento = documento
+    }
+}
+
+class Socios extends Personas{
+  #plan
+  #contraseña
+  constructor(id,nombre,apellido,correo,documento,plan,contraseña){
+    super(id,nombre,apellido,correo,documento),
+    this.#plan = plan,
+    this.#contraseña = contraseña
+  }
+}
 
 
 const nuevoSocioForm = document.getElementById("socioNuevoForm");
@@ -63,7 +87,7 @@ nuevoSocioForm.addEventListener("submit", function (event) {
 
   const ulErrores = document.getElementById("listaErrores");
   const mensaje = [];
-
+  var iconoError = '<i class="fa-solid fa-triangle-exclamation"></i> ';
   const { nombres, apellidos, correo, contraseña, contraseñaRep, terminos, tipoPlan, imagen } = nuevoSocioForm.elements;
 
   ulErrores.innerHTML = "";
@@ -71,58 +95,87 @@ nuevoSocioForm.addEventListener("submit", function (event) {
   //Nombres
   if (!nombres.value.trim()) {
     mensaje.push("Complete nombre.");
+    document.getElementById('lblNombreError').innerHTML = iconoError+'Complete su nombre.';
+  }else{
+    document.getElementById('lblNombreError').innerHTML = '';
   }
 
   //Apellidos
   if (!apellidos.value.trim()) {
     mensaje.push("Complete apellido.");
+    document.getElementById('lblApellidoError').innerHTML = iconoError+'Complete su apellido.';
+  }else{
+    document.getElementById('lblApellidoError').innerHTML = '';
   }
 
   //Correo
   if (!correo.value.trim() || !validarCorreo(correo.value)) {
     mensaje.push("Correo no válido.");
+    document.getElementById('lblEmailError').innerHTML = iconoError+'Correo no válido.';
+  }else{
+    document.getElementById('lblEmailError').innerHTML = '';
   }
 
   //Contraseña
   if (!contraseña.value.trim() || !validarContraseña(contraseña.value)) {
     mensaje.push("Contraseña debe ser de al menos de 8 carácteres, contener una mayúscula, una minúscula, un númro y un caracter especial.");
+    document.getElementById('lblPassError').innerHTML = iconoError+'Contraseña debe ser de al menos de 8 carácteres, contener una mayúscula, una minúscula, un númro y un caracter especial.';
+  }else{
+    document.getElementById('lblPassError').innerHTML = '';
   }
 
   if(!contraseñaRep.value.trim()){
     mensaje.push('Por favor repita su contraseña');
+    document.getElementById('lblrptPassError').innerHTML = iconoError+'Por favor repita su contraseña.';
   }else{
     if(contraseña.value != contraseñaRep.value){
       mensaje.push('Las contraseñas ingresadas no coinciden.');
+      document.getElementById('lblrptPassError').innerHTML = iconoError+'Las contraseñas ingresadas no coinciden.';
+    }else{
+      document.getElementById('lblrptPassError').innerHTML = '';
     }
   }
 
   //Terminos
   if(!terminos.checked){
     mensaje.push("Por favor acepte los términos y condiciones.");
+    document.getElementById('lblCondicionesError').innerHTML = iconoError+'Por favor acepte los términos y condiciones.';
+  }else{
+    document.getElementById('lblCondicionesError').innerHTML = '';
+  }
+
+  //tipoPlan
+  if(tipoPlan.value == "0"){
+    mensaje.push("Selecciona tu tipo de plan.");
+    document.getElementById('lblPlanError').innerHTML = iconoError+'Selecciona tu tipo de plan.';
+  }else{
+    document.getElementById('lblPlanError').innerHTML = '';
   }
 
   //archivo
-  if(tipoPlan.value == "0"){
-    mensaje.push("Selecciona tu tipo de plan.");
-  }
-  console.log(imagen.value);
   if(imagen.value != ""){
     //console.log('HAY')
     if(!validarTipoArchivo(imagen.value)){
       mensaje.push("Tipo de archivo permitido: png, jpg.");
+      document.getElementById('lblIMGError').innerHTML = iconoError+'Tipo de archivo permitido: png, jpg.';
+    }else{
+      document.getElementById('lblIMGError').innerHTML = '';
     }
   }else{
     //console.log('NO HAY')
     mensaje.push("Por favor suba una imagen de su documento.");
+    document.getElementById('lblIMGError').innerHTML = iconoError+'Por favor suba una imagen de su documento.';
   }
  
 
   if(mensaje.length > 0){
-    //console.log(mensaje);
-    ListarErrores(mensaje,ulErrores);
+    console.log(mensaje);
+    //ListarErrores(mensaje,ulErrores);
     return
   }else{
     //Todo correcto, mandamos formulario
+    var nuevoSocio = new Socios(1,nombres.value,apellidos.value,correo.value,imagen.value,tipoPlan.value, contraseña.value);
+    console.log(nuevoSocio);
     alert("Gracias por hacerte socio!");
     nuevoSocioForm.reset();
   }
